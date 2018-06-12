169399bbd2aa7b352c44014d3e62842fe7f1284b
joe <Joesalmon1985@gmail.com>
2018-06-12 21:44:16 +0100

    modified:   GamesRoom.java make code shorter

--- GamesRoom.java
+++ GamesRoom.java
@@ -3,24 +3,20 @@ import java.util.Scanner;
 
 public class GamesRoom {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String gameToPlay = "error";
+		GamesRoom gamesRoom = new GamesRoom();
 		System.out.println("Welcome to the games room, you can play Rock, Paper, Sicssors or take the last carrot. Would you like to play today?");
-		while (gameToPlay.equals("error")) {
-			gameToPlay = getHumanPlayerChoiceYN();
+		    String gameToPlay = gamesRoom.getHumanPlayerChoiceYN();
+		    while (!gameToPlay.toLowerCase().equals("y") || gameToPlay.toLowerCase().equals("n")) {
+		        System.out.println("Invalid. Please enter y or n");
+		        gameToPlay = gamesRoom.getHumanPlayerChoiceYN();
+		    }
+		    if (gameToPlay.toLowerCase().equals("y")) {
+		        System.out.println("Okay");
+		        gamesRoom.getHumanPlayerChoiceGame();
+		    } else if (gameToPlay.toLowerCase().equals("n")) {
+		        System.out.println("BORING!! Okay bye");
+		    }    
 		}
-		if (gameToPlay.equals("y")) {
-			System.out.println("Okay");
-			GamesRoom.getHumanPlayerChoiceGame ();
-		}
-		else if (gameToPlay.equals("n")) {
-			System.out.println("BORING!! Okay bye");
-		}
-		else {
-			System.out.println("I'm very confused!!");
-		}
-	
-	}
 	public class HumanPlayer {
 		String humanPlayerName, playChoice;
 		int humanGamesWon, humanGamesLost;
@@ -34,38 +30,32 @@ public class GamesRoom {
 	public static void getHumanPlayerChoiceGame () {
 		System.out.println("Okay so what game would you like to play?\n1. Rock, Paper, Scissors\n2. Take the last carrot");
 		String playerChoice = "error";
-		while (playerChoice.equals("error")) {
+		while (!playerChoice.matches("1|2|3")) {
 			playerChoice = GamesRoom.getHumanPlayerChoice123 ();
-		}
-		if (playerChoice.equals("1")) {
-			System.out.println("Okay, lets play Rock, Paper, Scissors.");
-			GameRockPaperScissors.main(null);
-		}
-		else if (playerChoice.equals("2")) {
-			System.out.println("Okay, lets play Take the last carrot.");
-			GameCarrot.main(null);
-		}
-		else {
-		System.out.println("Sadly I need more code");
+			if (playerChoice.equals("1")) {
+				System.out.println("Okay, lets play Rock, Paper, Scissors.");
+				GameRockPaperScissors.main(null);
+			}
+			else if (playerChoice.equals("2")) {
+				System.out.println("Okay, lets play Take the last carrot.");
+				GameCarrot.main(null);
+			}
+			else if (playerChoice.equals("3")) {
+				System.out.println("Sorry I need more code.");
+				GameCarrot.main(null);
+			}
 		}
 	}
+	
+	// This method is 10 lines long, I can't figure out how to split it into 5 lines.
 	public static String getHumanPlayerChoice123 () {
-		System.out.println("Enter 1, 2 or 3 only.");
 		Scanner keyboard = new Scanner(System.in);
-		String playerChoice;
-		playerChoice = keyboard.next();
-		if (playerChoice.equals("1")) {
+		String playerChoice = keyboard.next();
+		if (playerChoice.matches("1|2|3")) {
 			return playerChoice;
 			}
-		else if (playerChoice.equals("2")) {
-			return playerChoice;
-			}
-		else if (playerChoice.equals("3")) {
-			return playerChoice;
-			}
-
 		else {
-			System.out.println ("Sorry, that input was wrong.");
+			System.out.println ("Sorry, that input was wrong. Enter 1, 2 or 3 only.");
 			return "error";
 		}	
 	}
